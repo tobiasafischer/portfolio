9a79e694ee625c87f4b8992213f40b12b795df42
tobiasafischer <40577304+tobiasafischer@users.noreply.github.com>
2021-08-03 15:43:05 -0700

    scrapping projects

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ParallaxProvider } from 'react-scroll-parallax';
-
 import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
 	<React.StrictMode>
-		<ParallaxProvider>
-			<App />
-		</ParallaxProvider>
+		<App />
 	</React.StrictMode>,
 	document.getElementById('root'),
 );
