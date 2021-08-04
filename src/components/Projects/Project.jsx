9f22b94e648229842090836ef85701b372d9edea
tobiasafischer <40577304+tobiasafischer@users.noreply.github.com>
2021-08-04 15:14:05 -0700

    added dynamic prop passthrough for project as well as adjusted links for demo + code, also added description

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -3,21 +3,26 @@ import Overlay from './Overlay';
 import './Project.scss';
 
 const Project = (props) => {
+	const [{ title }] = useState(props);
+	const [{ date }] = useState(props);
+	const [{ description }] = useState(props);
+	const [{ gif }] = useState(props);
+	const [{ demo }] = useState(props);
+	const [{ code }] = useState(props);
+
 	return (
 		<div id='project'>
 			<div id='title-container'>
 				<h3>
-					<strong>Title</strong>
+					<strong>{title}</strong>
 				</h3>
 				<h3 style={{ fontSize: '0.9em', color: '#505050' }}>
-					<a>AUGUST 4th, 2021</a>
+					<a>{date}</a>
 				</h3>
 			</div>
 			<div id='container'>
 				<img
-					src={
-						'https://firebasestorage.googleapis.com/v0/b/portfolio-30db7.appspot.com/o/Animation.gif?alt=media&token=b1014180-3e3f-4ea6-887d-9b5f3839ad13'
-					}
+					src={gif}
 					onError={(e) => {
 						e.currentTarget.style = {};
 						e.currentTarget.src = 'https://bitsofco.de/content/images/2018/12/broken-1.png';
@@ -26,7 +31,10 @@ const Project = (props) => {
 					rounded
 					thumbnail='true'
 				/>
-				<Overlay />
+				<Overlay demo={demo} code={code} />
+			</div>
+			<div className='description'>
+				<p>{description}</p>
 			</div>
 		</div>
 	);
--- src/components/Projects/ProjectContainer.jsx
+++ src/components/Projects/ProjectContainer.jsx
@@ -4,7 +4,14 @@ import Project from './Project';
 const ProjectContainer = () => {
 	return (
 		<div id='projects'>
-			<Project />
+			<Project
+				title='Groupster'
+				date='June, 2021'
+				gif='https://firebasestorage.googleapis.com/v0/b/portfolio-30db7.appspot.com/o/Animation.gif?alt=media&token=b1014180-3e3f-4ea6-887d-9b5f3839ad13'
+				description='A chatroom application designed for bringing study groups together to chat and share files.'
+				demo='https://groupster-chat.herokuapp.com/'
+				code='https://github.com/h2Ocean/Groupster'
+			/>
 			<Project />
 			<Project />
 		</div>
